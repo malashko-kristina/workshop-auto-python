b0d750c28b4e819f4c4faf23f44f9293df3d1cc5
Kristina Malashko <kristina.malashko@pinch.nl>
2024-03-30 22:01:22 +0100

    Правки по Домашнему заданию #2 + CI/CD pipeline

--- pages/build_conf_detailed_page.py
+++ pages/build_conf_detailed_page.py
@@ -7,11 +7,11 @@ class ErrorMessageFragment(BasePage):
         self.page = page
         super().__init__(page)
         self.error_message_build_problem = '#buildProblemsPreview'
+        self.exception_sign = 'button[type="button"][title="exception"]'
 
     def check_error_on_screen(self):
-        with allure.step('Проверка наличия текста ошибки message_build_problem на странице'):
-            self.actions.wait_for_selector(self.error_message_build_problem)
-            self.actions.assert_text_in_element(self.error_message_build_problem, "1 Build Problem, 1 new")
+        with allure.step('Проверка наличия знака ошибки на странице'):
+            self.actions.wait_for_selector(self.exception_sign)
             self.actions.check_error_text_color(self.error_message_build_problem)
 
 
